86970ae2c5dac231f2b291149ec46a330efcf2b5
Jonas-BBW <jonas.bbw@yahoo.com>
2019-02-26 09:47:55 +0100

    Initial commit

---
+++ Testing/Fröhliche Zahlen/Fröhliche Zahlen/con_fröhlicheZahlen.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Fröhliche_Zahlen
+{
+    class con_fröhlicheZahlen
+    {
+        public static void Main(string[] args)
+        {
+            string zahl = Zahlabfrage();
+
+            while (zahl != "1" && zahl != "4")
+            {
+                zahl = Fröhlichkeitsprüfung(zahl);
+                Console.WriteLine(zahl);
+            }
+
+            zeigeErgebnis(zahl);
+        }
+
+        internal static string Zahlabfrage()
+        {
+            Console.WriteLine("Fröhlichkeitsprüfung");
+            Console.WriteLine("Bitte geben Sie eine Zahl ein!");
+            return Console.ReadLine();
+        }
+
+        internal static string Fröhlichkeitsprüfung(string zahl)
+        {
+            double rechnung = 0;
+
+            int[] zahlenIntArray = new int[zahl.Length];
+            for (int i = 0; i < zahl.Length; i++)
+            {
+                zahlenIntArray[i] = int.Parse(zahl[i].ToString());
+            }
+
+            for (int i = 0; i <= zahlenIntArray.Length; i++)
+            {
+                try
+                {
+                    rechnung = rechnung + Math.Pow(zahlenIntArray[i], 2);
+                }
+                catch
+                {
+                    rechnung = rechnung + 0;
+                }
+            }
+            return rechnung.ToString();
+        }
+
+        internal static void zeigeErgebnis(string zahl)
+        {
+            switch(zahl)
+            {
+                case "1": MessageBox.Show("Diese Zahl ist fröhlich.", "Ergebnis", MessageBoxButtons.OK, MessageBoxIcon.Information); break;
+                case "4": MessageBox.Show("Diese Zahl ist traurig.", "Ergebnis", MessageBoxButtons.OK, MessageBoxIcon.Information); break;
+            }
+        }
+    }
+}
---
+++ Testing/Fröhliche Zahlen/NUnit/UnitTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Fröhliche_Zahlen;
+
+namespace NUnit
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [Test, Category("Gerüsttest")]
+        public void FröhlicheZahl()
+        {
+            string variable = con_fröhlicheZahlen.Fröhlichkeitsprüfung("100");
+            Assert.AreEqual(variable, "1");
+        }
+
+        [Test, Category("Gerüsttest")]
+        public void TraurigeZahl()
+        {
+            string variable = con_fröhlicheZahlen.Fröhlichkeitsprüfung("2");
+            Assert.AreEqual(variable, "4");
+        }
+    }
+}
